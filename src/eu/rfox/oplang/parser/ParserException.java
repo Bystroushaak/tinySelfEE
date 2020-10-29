49acf4bba818f917ae18f63aeaa7c4b5c68fee52
Bystroushaak <bystrousak@kitakitsune.org>
2020-10-29 03:43:30 +0100

    Added token position info to the ParserException. #4

--- src/eu/rfox/oplang/parser/ParserException.java
+++ src/eu/rfox/oplang/parser/ParserException.java
@@ -1,4 +1,18 @@
 package eu.rfox.oplang.parser;
 
+import eu.rfox.oplang.tokenizer.Token;
+
 public class ParserException extends Exception {
+    Token token_start;
+    Token token_end;
+
+    ParserException(String message) {
+        super(message);
+    }
+
+    ParserException(String message, Token token_start, Token token_end) {
+        super(message);
+        this.token_start = token_start;
+        this.token_end = token_end;
+    }
 }
