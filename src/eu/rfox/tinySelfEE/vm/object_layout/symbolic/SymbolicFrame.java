08293d926d21d5e583e7c11dca471b5ea26c08a1
Bystroushaak <bystrousak@kitakitsune.org>
2021-03-05 01:24:32 +0100

    Added just a skeleton of SymbolicFrame.

---
+++ src/eu/rfox/tinySelfEE/vm/object_layout/symbolic/SymbolicFrame.java
@@ -0,0 +1,9 @@
+package eu.rfox.tinySelfEE.vm.object_layout.symbolic;
+
+import eu.rfox.tinySelfEE.vm.object_layout.ObjectRepr;
+
+public class SymbolicFrame {
+    public void pushObject(ObjectRepr obj) {
+
+    }
+}
