1d0d98b37db0dc50df113692d54f537a36c6136d
Bystroushaak <bystrousak@kitakitsune.org>
2021-07-11 01:50:02 +0200

    Added new proxy object for Block representation.

---
+++ src/eu/rfox/tinySelfEE/vm/object_layout/BlockRepr.java
@@ -0,0 +1,18 @@
+package eu.rfox.tinySelfEE.vm.object_layout;
+
+public class BlockRepr extends ObjectRepr {
+    public ObjectRepr surrounding_obj;
+
+    public BlockRepr() {
+        super();
+    }
+
+    public ObjectInterface clone() {
+        BlockRepr b = new BlockRepr();
+        super.clone(b);
+
+        b.surrounding_obj = surrounding_obj;
+
+        return b;
+    }
+}
